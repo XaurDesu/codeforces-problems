7e59797f250d4e8d1145dd805a750de280590790
Jaime Torres <andrestbermejoj@gmail.com>
2023-04-10 11:20:28 -0500

    add scaffolding

---
+++ codeforces-problems/target/debug/deps/codeforces_problems-9a3c97d9b1dd1aae.d
@@ -0,0 +1,5 @@
+/home/xaurdesu/Documentos/repos/codeforces-problems/codeforces-problems/target/debug/deps/codeforces_problems-9a3c97d9b1dd1aae.rmeta: src/main.rs
+
+/home/xaurdesu/Documentos/repos/codeforces-problems/codeforces-problems/target/debug/deps/codeforces_problems-9a3c97d9b1dd1aae.d: src/main.rs
+
+src/main.rs:
---
+++ codeforces-problems/target/debug/deps/codeforces_problems-a1f70b358a1ac73a.d
@@ -0,0 +1,5 @@
+/home/xaurdesu/Documentos/repos/codeforces-problems/codeforces-problems/target/debug/deps/codeforces_problems-a1f70b358a1ac73a.rmeta: src/main.rs
+
+/home/xaurdesu/Documentos/repos/codeforces-problems/codeforces-problems/target/debug/deps/codeforces_problems-a1f70b358a1ac73a.d: src/main.rs
+
+src/main.rs:
